c367feffe07363dd7bf0fa357b50eed1ac84c084
Peter Mei <pmei@senegence.com>
2021-06-09 16:03:17 -0700

    9-4 Add handleSubmit

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -12,9 +12,14 @@ class CoursesPage extends React.Component {
     this.setState({ course: course });
   };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
         <h3>Add Courses</h3>
         <input
