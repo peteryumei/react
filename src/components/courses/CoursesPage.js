baaef6b261f3d6d3808142cb35b2ede681c2218c
Peter Mei <pmei@senegence.com>
2021-06-10 16:52:53 -0700

    9-7 Add bindActionCreators

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 
 class CoursesPage extends React.Component {
   state = {
@@ -18,7 +19,7 @@ class CoursesPage extends React.Component {
   handleSubmit = (event) => {
     event.preventDefault();
     //debugger;
-    this.props.createCourse(this.state.course);
+    this.props.actions.createCourse(this.state.course);
     //alert(this.state.course.title);
   };
 
@@ -43,7 +44,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
-  createCourse: PropTypes.func.isRequired,
+  actions: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state) {
@@ -53,7 +54,7 @@ function mapStateToProps(state) {
 }
 function mapDispatchToProps(dispatch) {
   return {
-    createCourse: (course) => dispatch(courseActions.createCourse(course)),
+    actions: bindActionCreators(courseActions, dispatch),
   };
 }
 
