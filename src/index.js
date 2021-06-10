085eaf18059e1771e94e50bc28554382f0966585
Peter Mei <pmei@senegence.com>
2021-06-09 20:06:32 -0700

    9-5 Add redux action, reducer and configureStore.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as reduxProvider } from "react-redux";
+
+const store = configureStore();
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <reduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </reduxProvider>,
   document.getElementById("app")
 );
