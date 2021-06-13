29d80fe319df3a45171fcbad22e608a9b9db712b
gitbear <petermei@yahoo.com>
2021-06-12 18:34:51 -0700

    11-2 Convert class component to function component

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,33 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 
-class ManageCoursesPage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, loadCourses, loadAuthors } = this.props;
+function ManageCoursesPage({ courses, authors, loadCourses, loadAuthors })  {
+    useEffect(() => {
+        if (courses.length === 0) {
+            loadCourses().catch(error => {
+                alert("Loading courses failed" + error);
+            });
+        }
 
-    if (courses.length === 0) {
-      loadCourses().catch(error => {
-        alert("Loading courses failed" + error);
-      });
-    }
+        if (authors.length === 0) {
+            loadAuthors().catch(error => {
+                alert("Loading authors failed" + error);
+            }); 
+        }
+    }, []);
 
-    if (authors.length === 0) {
-      loadAuthors().catch(error => {
-        alert("Loading authors failed" + error);
-    }); 
-  }
-  }
-
-  render() {
     return (
      <>
         <h2>Manage Courses</h2>
       </>
     );
-  }
 }
 
 ManageCoursesPage.propTypes = {
